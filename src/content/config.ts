6d022f6f802d04631eb312b88a0218d225196354
hkaramon <hynek.karamon@gmail.com>
2023-12-03 11:06:59 +0100

    fotogalerie-update

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,45 +3,20 @@ import { defineCollection, z } from "astro:content";
 // Homepage schema
 const homepage = defineCollection({
   schema: z.object({
-    banner: z.object({
+    intro: z.object({
       title: z.string().optional(),
       content: z.string().optional(),
       image: z.string().optional(),
-      button: z.object({
-        label: z.string(),
-        link: z.string().default("#"),
-        enable: z.boolean().default(true)
-      })
-    }).optional(),
-    feature: z.object({
-      title: z.string().optional(),
-      features: z.array(z.object({name: z.string().optional(), icon: z.string().optional(), content: z.string().optional()})),
-    }).optional(),
-    services: z.array(z.object({
-      title: z.string().optional(),
-      content: z.string().optional(),
-      images: z.array(z.string()).optional(),
-      button: z.object({
-        label: z.string(),
-        link: z.string().default("#"),
-        enable: z.boolean().default(true)
-      }).optional()
-    })).optional(),
-    workflow: z.object({
-      title: z.string().optional(),
-      description: z.string().optional(),
-      image: z.string()
-    }).optional(),
-    call_to_action: z.object({
-      title: z.string().optional(),
-      content: z.string().optional(),
-      image: z.string(),
-      button: z.object({
-        label: z.string(),
-        link: z.string().default("#"),
-        enable: z.boolean().default(true)
-      }).optional()
-    }).optional()
+    }),
+    sections: z
+      .array(
+        z.object({
+          title: z.string().optional(),
+          content: z.string().optional(),
+          image: z.string().optional(),
+        }),
+      )
+      .optional(),
   }),
 });
 
@@ -78,13 +53,15 @@ const contact_page = defineCollection({
     title: z.string().optional(),
     description: z.string().optional(),
     draft: z.boolean().optional(),
-    info: z.object({
-      title: z.string().optional(),
-      description: z.string().optional(),
-      contacts: z.array(z.string()).optional()
-    }).optional()
-  })
-})
+    info: z
+      .object({
+        title: z.string().optional(),
+        description: z.string().optional(),
+        contacts: z.array(z.string()).optional(),
+      })
+      .optional(),
+  }),
+});
 
 //faq page schema
 const faq_page = defineCollection({
@@ -92,12 +69,16 @@ const faq_page = defineCollection({
     title: z.string().optional(),
     description: z.string().optional(),
     draft: z.boolean().optional(),
-    faqs: z.array(z.object({
-      title: z.string(),
-      answer: z.string(),
-    })).optional()
-  })
-})
+    faqs: z
+      .array(
+        z.object({
+          title: z.string(),
+          answer: z.string(),
+        }),
+      )
+      .optional(),
+  }),
+});
 
 //pricing page schema
 const pricing_page = defineCollection({
@@ -105,48 +86,72 @@ const pricing_page = defineCollection({
     title: z.string().optional(),
     description: z.string().optional(),
     draft: z.boolean().optional(),
-    plans: z.array(z.object({
-      title: z.string(),
-      subtitle: z.string(),
-      price: z.number(),
-      recommended: z.boolean().optional(),
-      type: z.string(),
-      features: z.array(z.string()).optional(),
-      button: z.object({
-        label: z.string(),
-        link: z.string().default("#"),
-        enable: z.boolean().default(true)
+    plans: z
+      .array(
+        z.object({
+          title: z.string(),
+          subtitle: z.string(),
+          price: z.number(),
+          recommended: z.boolean().optional(),
+          type: z.string(),
+          features: z.array(z.string()).optional(),
+          button: z.object({
+            label: z.string(),
+            link: z.string().default("#"),
+            enable: z.boolean().default(true),
+          }),
+        }),
+      )
+      .optional(),
+    call_to_action: z
+      .object({
+        title: z.string().optional(),
+        content: z.string().optional(),
+        image: z.string(),
+        button: z
+          .object({
+            label: z.string(),
+            link: z.string().default("#"),
+            enable: z.boolean().default(true),
+          })
+          .optional(),
       })
-    })).optional(),
-    call_to_action: z.object({
-      title: z.string().optional(),
-      content: z.string().optional(),
-      image: z.string(),
-      button: z.object({
-        label: z.string(),
-        link: z.string().default("#"),
-        enable: z.boolean().default(true)
-      }).optional()
-    }).optional()
-  })
-})
+      .optional(),
+  }),
+});
+
+const fotogalerie = defineCollection({
+  schema: z.object({
+    years: z
+      .array(
+        z.object({
+          title: z.any(),
+          image1: z.string(),
+          image2: z.string(),
+          remainingPhotosLink: z.string(),
+        }),
+      )
+      .optional(),
+  }),
+});
 
 // Interface for content
 export interface PageData {
-  blog: string,
-  pages: string,
-  // homePage: string,
+  blog: string;
+  pages: string;
+  fotogalerie: string;
+  homePage: string;
   // contact: string,
   // faq: string,
   // pricing: string
 }
 
-
 // Export collections
 export const collections = {
   blog: postsCollection,
   pages: pagesCollection,
-  // homepage: homepage,
+  fotogalerie: fotogalerie,
+  homepage: homepage,
   // contact: contact_page,
   // faq: faq_page,
   // pricing: pricing_page
--- src/content/fotogalerie/index.md
+++ src/content/fotogalerie/index.md
@@ -1,113 +1,77 @@
 ---
 title: Fotogalerie
-
+layout: ../../
 years:
   - title: Poslední tábor
     image1: /images/group-1-2023.jpg
-      url: /aaa
     image2: /images/group-2-2023.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2022
     image1: /images/group-1-2022.jpg
-      url: /aaa
     image2: /images/group-2-2022.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2021
     image1: /images/group-1-2021.jpg
-      url: /aaa
     image2: /images/group-2-2021.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2020
     image1: /images/group-1-2020.jpg
-      url: /aaa
     image2: /images/group-2-2020.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2019
     image1: /images/group-1-2019.jpg
-      url: /aaa
     image2: /images/group-2-2019.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2018
     image1: /images/group-1-2018.jpg
-      url: /aaa
     image2: /images/group-2-2018.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2017
     image1: /images/group-1-2017.jpg
-      url: /aaa
     image2: /images/group-2-2017.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2016
     image1: /images/group-1-2016.jpg
-      url: /aaa
     image2: /images/group-2-2016.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
-   - title: 2015
+  - title: 2015
     image1: /images/group-1-2015.jpg
-      url: /aaa
     image2: /images/group-2-2015.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
-   - title: 2011
+  - title: 2011
     image1: /images/group-1-2011.jpg
-      url: /aaa
     image2: /images/group-2-2011.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
-   - title: 2010
+  - title: 2010
     image1: /images/group-1-2010.jpg
-      url: /aaa
     image2: /images/group-2-2010.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
-   - title: 2009
+  - title: 2009
     image1: /images/group-1-2009.jpg
-      url: /aaa
     image2: /images/group-2-2009.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2008
     image1: /images/group-1-2008.jpg
-      url: /aaa
     image2: /images/group-2-2008.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2007
     image1: /images/group-1-2007.jpg
-      url: /aaa
     image2: /images/group-2-2007.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2006
     image1: /images/group-1-2006.jpg
-      url: /aaa
     image2: /images/group-2-2006.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2005
     image1: /images/group-1-2005.jpg
-      url: /aaa
     image2: /images/group-2-2005.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2004
     image1: /images/group-1-2004.jpg
-      url: /aaa
     image2: /images/group-2-2004.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
   - title: 2003
     image1: /images/group-1-2003.jpg
-      url: /aaa
     image2: /images/group-2-2003.jpg
-      url: /bbb
     remainingPhotosLink: /aaaa
 ---
